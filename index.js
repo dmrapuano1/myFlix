ef86611d7862c4819245df8964e184e650e3c98a
dmrapuano1 <dmrapuano1@gmail.com>
2020-02-29 13:26:08 -0500

    updated topMovies to have nested objects for genres and directors

--- index.js
+++ index.js
@@ -13,66 +13,168 @@ app.use(bodyParser.json());
 // Creates file of Top 10 Movies
 let topMovies = [
     {
-        title: 'Wedding Ringer',
-        genre: 'Comedy',
+        title: 'The Wedding Ringer',
+        genre: {
+            name: 'Comedy',
+            description: 'Comedy is a genre of film in which the main emphasis is on humor. These films are designed to make the audience laugh through amusement and most often work by exaggerating characteristics for humorous effect.'
+        },
         year: '2015',
-        director: 'Jeremy Garelick'
+        director: {
+            name: 'Jeremy Garelick',
+            dob: '1975-11-30',
+            date_died: null,
+            bio: 'Jeremy graduated from Yale with degrees in Film and Theater. He began his career working as an assistant in Creative Artists Agency\'s Motion Picture Literary department before joining director Joel Schumacher as his assistant on the sets of the critically acclaimed Tigerland, Jerrys Bruckheimer\'s Bad Company.'
+        },
+        description: "Two weeks shy of his wedding, a socially awkward guy enters into a charade by hiring the owner of a company that provides best men for grooms in need.",
+        featured: false
     },
     {
-        title: 'The Matrix',
-        genre: 'Sci-fi',
-        year: '1999',
-        director: ['Lana Wachowski', 'Lilly Wachowski']
+        title: 'Jaws',
+        genre: {
+            name: 'Thriller',
+            description: 'Thriller is a genre of fiction, having numerous, often overlapping subgenres. Thrillers are characterized and defined by the moods they elicit, giving viewers heightened feelings of suspense, excitement, surprise, anticipation and anxiety.'
+        },
+        year: '1975',
+        director: {
+            name: 'Steven Spielberg',
+            dob: '1946-12-18',
+            date_died: null,
+            bio: 'Steven Allan Spielberg is an American filmmaker. He is considered one of the founding pioneers of the New Hollywood era and one of the most popular directors and producers in film history',
+        },
+        description: "When a killer shark unleashes chaos on a beach community, it's up to a local sheriff, a marine biologist, and an old seafarer to hunt the beast down.",
+        featured: false
     },
     {
         title: 'The Fast and the Furious',
-        genre: 'Action',
+        genre: {
+            name: 'Action',
+            description: 'Action film is a film genre in which the protagonist or protagonists are thrust into a series of events that typically include violence, extended fighting, physical feats, and frantic chases. Action films tend to feature a resourceful hero struggling against incredible odds, which include life-threatening situations, a villain, or a pursuit which usually concludes in victory for the hero (though a small number of films in this genre have ended in the victory for the villain instead).',
+        },
         year: '2001',
-        director: 'Rob Cohen'
+        director: {
+            name: 'Rob Cohen',
+            dob: '1949-03-12',
+            date_died: null,
+            bio: 'Rob L. Cohen is an American director, producer, and screenwriter of film and television. Beginning his career as an executive producer at 20th Century Fox, Cohen produced and developed numerous high-profile film and television programs, including The Wiz, The Witches of Eastwick, and Light of Day, before concentrating full-time on directing in the 1990s.'
+        },
+        description: "Los Angeles police officer Brian O'Conner must decide where his loyalty really lies when he becomes enamored with the street racing world he has been sent undercover to destroy.",
+        featured: false
     },
     {
         title: 'Rocky',
-        genre: 'Drama',
+        genre: {
+            name: 'Drama',
+            description: 'Drama is a genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone. Drama of this kind is usually qualified with additional terms that specify its particular subgenre, such as "police crime drama", "political drama", "legal drama", "historical drama", "domestic drama", "teen drama" or "comedy-drama". These terms tend to indicate a particular setting or subject-matter, or else they qualify the otherwise serious tone of a drama with elements that encourage a broader range of moods.',
+        },
         year: '1976',
-        director: 'John G. Avildsen'
+        director: {
+            name: 'John G. Avildsen',
+            dob: '1935-12-21',
+            date_died: '2017-06-16',
+            bio: 'John Guilbert Avildsen was an American film director. He is perhaps best known for directing Rocky (1976), which earned him the Academy Award for Best Director.'
+        },
+        description: "A small-time boxer gets a supremely rare chance to fight a heavy-weight champion in a bout in which he strives to go the distance for his self-respect.",
+        featured: false
     },
     {
         title: 'Up',
-        genre: 'Drama',
+        genre: {
+            name: 'Animated',
+            description: 'Animation is a method in which pictures are manipulated to appear as moving images. In traditional animation, images are drawn or painted by hand on transparent celluloid sheets to be photographed and exhibited on film.'
+        },
         year: '2009',
-        director: 'Pete Docter'
+        director: {
+            name: 'Pete Docter',
+            dob: '1968-10-09',
+            date_died: null,
+            bio: 'Peter Hans Docter is an American animator, film director, screenwriter, producer, voice actor and chief creative officer of Pixar. He is best known for directing the animated feature films Monsters, Inc. (2001), Up (2009), Inside Out (2015) and the upcoming Soul (2020), and as a key figure and collaborator at Pixar. He has been nominated for eight Oscars (two wins thus far for Up and Inside Out – Best Animated Feature), seven Annie Awards (winning five), a BAFTA Children\'s Film Award (which he won) and a Hochi Film Award (which he won).'
+        },
+        description: "78-year-old Carl Fredricksen travels to Paradise Falls in his house equipped with balloons, inadvertently taking a young stowaway.",
+        featured: false
     },
     {
         title: 'Inception',
-        genre: 'Thriller',
+        genre: {
+            name: 'Thriller',
+            description: 'Thriller is a genre of fiction, having numerous, often overlapping subgenres. Thrillers are characterized and defined by the moods they elicit, giving viewers heightened feelings of suspense, excitement, surprise, anticipation and anxiety.'
+        },
         year: '2010',
-        director: 'Christopher Nolan'
+        director: {
+            name: 'Christopher Nolan',
+            dob: '1970-07-30',
+            date_died: null,
+            bio: 'Christopher Edward Nolan, is a British-American filmmaker, who is known for making personal, distinctive films within the Hollywood mainstream. His ten films have grossed over US$4.7 billion worldwide and garnered a total of 34 Oscar nominations and ten wins.'
+        },
+        description: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+        featured: false
     },
     {
         title: 'The Breakfast Club',
-        genre: 'Drama',
+        genre: {
+            name: 'Drama',
+            description: 'Drama is a genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone. Drama of this kind is usually qualified with additional terms that specify its particular subgenre, such as "police crime drama", "political drama", "legal drama", "historical drama", "domestic drama", "teen drama" or "comedy-drama". These terms tend to indicate a particular setting or subject-matter, or else they qualify the otherwise serious tone of a drama with elements that encourage a broader range of moods.',
+        },
         year: '1985',
-        director: 'John Hughes'
+        director: {
+            name: 'John Hughes',
+            dob: '1950-02-18',
+            date_died: '2009-08-06',
+            bio: 'John Wilden Hughes Jr. was an American filmmaker. Beginning as an author of humorous essays and stories for National Lampoon, he went on to write, produce and sometimes direct some of the most successful live-action comedy films of the 1980s and 1990s.'
+        },
+        description: "Five high school students meet in Saturday detention and discover how they have a lot more in common than they thought.",
+        featured: false
     },
     {
         title: 'Schindler\'s List',
-        genre: 'Drama',
+        genre: {
+            name: 'Drama',
+            description: 'Drama is a genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone. Drama of this kind is usually qualified with additional terms that specify its particular subgenre, such as "police crime drama", "political drama", "legal drama", "historical drama", "domestic drama", "teen drama" or "comedy-drama". These terms tend to indicate a particular setting or subject-matter, or else they qualify the otherwise serious tone of a drama with elements that encourage a broader range of moods.',
+        },
         year: '1993',
-        director: 'Steven Spielberg'
+        director: {
+            name: 'Steven Spielberg',
+            dob: '1946-12-18',
+            date_died: null,
+            bio: 'Steven Allan Spielberg is an American filmmaker. He is considered one of the founding pioneers of the New Hollywood era and one of the most popular directors and producers in film history',
+        },
+        description: "In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.",
+        featured: false
     },
     {
         title: 'The Shining',
-        genre: 'Horror',
+        genre: {
+            name: 'Horror',
+            description: 'Horror is a genre of speculative fiction which is intended to frighten, scare, disgust, or startle its viewers by inducing feelings of horror and terror. Horror is frequently supernatural, though it might be also non-supernatural. Often the central menace of a work of horror fiction can be interpreted as a metaphor for the larger fears of a society.'
+        },
         year: '1980',
-        director: 'Stanley Kubrick'
+        director: {
+            name: 'Stanley Kubrick',
+            dob: '1928-07-26',
+            date_died: '1999-03-07',
+            bio: 'Stanley Kubrick was an American film director, screenwriter, and producer. He is frequently cited as one of the most influential filmmakers in cinematic history. His films, which are mostly adaptations of novels or short stories, cover a wide range of genres, and are noted for their realism, dark humor, unique cinematography, extensive set designs, and evocative use of music.'
+        },
+        description: "A family heads to an isolated hotel for the winter where a sinister presence influences the father into violence, while his psychic son sees horrific forebodings from both past and future.",
+        featured: false
     },
     {
         title: 'Groundhog Day',
-        genre: 'Drama',
+        genre: {
+            name: 'Drama',
+            description: 'Drama is a genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone. Drama of this kind is usually qualified with additional terms that specify its particular subgenre, such as "police crime drama", "political drama", "legal drama", "historical drama", "domestic drama", "teen drama" or "comedy-drama". These terms tend to indicate a particular setting or subject-matter, or else they qualify the otherwise serious tone of a drama with elements that encourage a broader range of moods.',
+        },
         year: '1993',
-        director: 'Harold Ramis'
+        director: {
+            name: 'Harold Ramis',
+            dob: '1944-11-21',
+            date_died: '2014-02-24',
+            bio: 'Harold Allen Ramis was an American actor, comedian, director and writer. His best-known film acting roles were as Egon Spengler in Ghostbusters (1984) and Ghostbusters II (1989) and Russell Ziskey in Stripes (1981); he also co-wrote those films.'
+        },
+        description: "A weatherman finds himself inexplicably living the same day over and over again.",
+        featured: false
     },
 ];
+
+//Creates list of users
 
 // Middleware that logs all navigation to webpage
 app.use(morgan('common'));
