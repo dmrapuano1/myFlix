a7cf539b545dfc72b15b4c9c260a48cb2ed88e11
dmrapuano1 <dmrapuano1@gmail.com>
2020-03-07 15:07:33 -0500

    updated access headers

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -33215,7 +33215,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60324" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61193" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
