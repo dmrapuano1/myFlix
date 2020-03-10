e852944b79bfeabfbd8ff3b3bea675f6e1d7648d
dmrapuano1 <dmrapuano1@gmail.com>
2020-03-10 15:47:32 -0400

    added director-card functionality

--- client/src/components/director-view/director-card.jsx
+++ client/src/components/director-view/director-card.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 import {Link} from 'react-router-dom';
 
-export class MovieView extends React.Component {
+export class DirectorCard extends React.Component {
   constructor() {
     super();
 
@@ -17,20 +17,24 @@ export class MovieView extends React.Component {
     if (!movie) return null;
 
     return (
-      <div className="movie-group">
-        <div className="movie-title">
-          <span className="label">Title: </span>
+      <div className="director-group">
+        <div className="director-name">
+          <span className="label">Name: </span>
           <span className="value">{movie.director.title}</span>
         </div>
-        <div className="movie-description">
+        <div className="director-bio">
           <span className="label">Biography: </span>
           <span className="value">{movie.director.bio}</span>
         </div>
-        <div className="movie-genre">
-          <span className="label">Years of life </span>
-          <span className="value">`{movie.director.dob} - {movie.director.date_died}`</span>
+        <div className="director-dob">
+          <span className="label">Date of Birth </span>
+          <span className="value">{movie.director.dob}</span>
         </div>
-        <Link to={`/`}>
+        <div className="director-death-day">
+          <span className="label">Date Died </span>
+          <span className="value">{movie.director.date_died}</span>
+        </div>
+        <Link to={`/directors`}>
           <Button variant="primary">Back</Button>
         </Link>
       </div>
