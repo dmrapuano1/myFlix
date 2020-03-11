c25e03d0b4d636c1f9fa95be2d8d7797007f7bc8
dmrapuano1 <dmrapuano1@gmail.com>
2020-03-11 00:08:46 -0400

    created outline of final look for profile-view

---
+++ client/src/components/profile-view/profile-view.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+import {Link} from 'react-router-dom';
+
+require('./profile-view.scss');
+
+export class ProfileView extends React.Component {
+
+  render() {
+    const {movie} = this.props
+
+    return (
+      <Card border="info" style={{ width: '16rem' }}>
+        <Card.Img variant="top" src={movie.imagePath} />
+        <Card.Body>
+          <Card.Title>{movie.title}</Card.Title>
+          <Card.Text>{movie.description}</Card.Text>
+          <Link to={`/movies/${movie._id}`}>
+            <Button variant="info">Details</Button>
+          </Link>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
+
+ProfileView.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired
+  }).isRequired,
+}
